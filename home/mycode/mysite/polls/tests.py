cf5398351dc8ab7ce44f70c2e03d9337f2d967e6
olivialabath <olivialabath@gmail.com>
2018-07-18 13:26:37 -0700

    added test case for questions published in the future

--- home/mycode/mysite/polls/tests.py
+++ home/mycode/mysite/polls/tests.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.utils import timezone
+from django.test import TestCase
+
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """was_published_recently() returns false for questions who pub_date is in the future"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(futureQuestion.was_published_recently(), False)
--- home/mycode/mysite/polls/views.py
+++ home/mycode/mysite/polls/views.py
@@ -8,6 +8,7 @@ from django.views import generic
 
 from .models import Question, Choice
 
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
@@ -16,13 +17,16 @@ class IndexView(generic.ListView):
             """Return the last five published questions."""
             return Question.objects.order_by('-pub_date')[:5]
 
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -31,7 +35,7 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         # redisplay the question voting form
         return render(request, 'polls/detail.html', {
-            'question': question, 
+            'question': question,
             'error_message': "You didn't select a choice.",
         })
     else:
@@ -40,5 +44,7 @@ def vote(request, question_id):
         # always return an HttpResponseRedirect after successfully dealing
         # with POST data. this prevents data from being posted twice if a
         # user hits the back button
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(
+            reverse('polls:results', args=(question.id,))
+        )
 
